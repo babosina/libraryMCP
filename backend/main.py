2758ef53e6b1c4e7df3b21da095a11b33b630312
babosina <babos77@gmail.com>
2026-02-18 15:31:31 +0400

    add db creating to app initialization

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
-
+from backend.database import engine, Base
+from backend.models import Book, Member, Loan
 
 app = FastAPI(title="LibraryMCP")
 
+Base.metadata.create_all(bind=engine)

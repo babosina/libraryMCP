cb02ebdb9b5958a118e9a3750e6ce6a1ec5852c8
babosina <babos77@gmail.com>
2026-03-27 16:45:58 +0400

    minor fix

--- mcp_server/main_stdio.py
+++ mcp_server/main_stdio.py
@@ -2,7 +2,7 @@
 This module provides a standard I/O interface to run the MCP server.
 It imports the MCP server instance and starts its run loop using stdio communication.
 """
-from .server import mcp
+from server import mcp
 
 
 if __name__ == "__main__":
